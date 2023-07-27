02c88bfbc807e8879fbcbcb2b1d17b9916679985
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-07-27 08:34:57 +0200

    we include the session variable

--- server.js
+++ server.js
@@ -1,78 +1,67 @@
-const express = require('express'); // Require Express module
-const session = require('express-session'); // Require Express session module
-const path = require('path');
-const bodyParser = require('body-parser'); // Require body-parser module
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static('public'));
 
-// Add session middleware
 app.use(session({
     secret: 'node_tutorial',
     resave: true,
     saveUninitialized: true
 }));
 
-// Middleware to parse JSON data
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// In-memory array to store notes
-let notes = []; // Your array of notes (already defined)
+app.use(function (req, res, next) {
+    if (!req.session.notes) {
+        req.session.notes = [];
+    }
+    next();
+});
 
-// Function to fetch notes from the in-memory array (no actual database needed)
-function getNotesFromDatabase() {
-  return new Promise((resolve) => {
-    // Assuming that the notes are already available in the 'notes' array
-    resolve(notes);
-  });
+function getNotesFromDatabase(req) {
+    return req.session.notes || [];
 }
 
-// Now you can define your routes and other configurations for the app
 app.get("/notes", async function (req, res) {
-  try {
-    const notes = await getNotesFromDatabase();
-    console.log("reading notes", notes);
-    res.send(notes);
-  } catch (error) {
-    console.error("Error reading notes", error);
-    res.status(500).send("Error reading notes");
-  }
+    try {
+        const notes = getNotesFromDatabase(req);
+        console.log("reading notes", notes);
+        res.send(notes);
+    } catch (error) {
+        console.error("Error reading notes", error);
+        res.status(500).json({ error: "Error reading notes" });
+    }
 });
 
 app.post("/notes", async function (req, res) {
     try {
-        let note = req.body.note; // Assuming the request contains the 'note' field in the body
-        notes.push({ id: Date.now(), text: note });
-        console.log("added note", notes);
+        let note = req.body.note;
+        req.session.notes.push({ id: Date.now(), text: note });
+        console.log("added note", req.session.notes);
         res.end();
     } catch (err) {
-        // Handle any potential errors here
         console.error("Error occurred:", err);
-        res.status(500).send("Something went wrong.");
+        res.status(500).json({ error: "Something went wrong." });
     }
 });
 
 app.delete("/notes/:id", async function (req, res) {
     try {
         const noteId = parseInt(req.params.id);
-
-        // Simulating an asynchronous operation using setTimeout
         await new Promise((resolve) => setTimeout(resolve, 1000));
-
-        notes = notes.filter((note) => note.id !== noteId);
-        console.log("deleted note", notes);
+        req.session.notes = req.session.notes.filter((note) => note.id !== noteId);
+        console.log("deleted note", req.session.notes);
         res.end();
     } catch (err) {
-        // Handle any potential errors here
         console.error("Error occurred:", err);
-        res.status(500).send("Something went wrong.");
+        res.status(500).json({ error: "Something went wrong." });
     }
 });
 
-// Start the server
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
